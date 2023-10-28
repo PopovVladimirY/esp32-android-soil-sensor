30a5871a7ef0d70a1a70f5f224a51863b5e7b361
Vladimir Popov <vladimir.popov@vitec.com>
2023-10-28 09:40:01 +0200

    merging stats in on notification

--- BLE_notify_W55.ino
+++ BLE_notify_W55.ino
@@ -27,40 +27,46 @@
 #include <Wire.h>
 #include <Adafruit_BME280.h>
 
+#define BATTERY_IO  34
+
+#define MSENSOR_N 10
+#define MSENSOR_IO 35
+
 #define MIN_MOISTURE 0 // 14-bit ADC range
 #define MAX_MOISTURE 4095 // 14-bit ADC range
 #define MMIN (MIN_MOISTURE)// + 4*180)
 #define MMAX (MAX_MOISTURE)// - 4*440)
 
 #define uS_TO_S_FACTOR 1000000  /* Conversion factor for micro seconds to seconds */
-#define TIME_TO_SLEEP  25        /* Time ESP32 will go to sleep (in seconds) */
+#define TIME_TO_SLEEP  20 //(60*10)        /* Time ESP32 will go to sleep (in seconds) */
 
-#define MAX_CONNECT_WAIT  10
+#define MAX_CONNECT_WAIT  100 // in 100ms intervals
 
 RTC_DATA_ATTR int bootCount = 0;
 
 BLEServer* pServer = NULL;
 BLECharacteristic* pCharacteristicT = NULL;
-BLECharacteristic* pCharacteristicP = NULL;
-BLECharacteristic* pCharacteristicH = NULL;
-BLECharacteristic* pCharacteristicB = NULL;
-BLECharacteristic* pCharacteristicM = NULL;
+//BLECharacteristic* pCharacteristicP = NULL;
+//BLECharacteristic* pCharacteristicH = NULL;
+//BLECharacteristic* pCharacteristicB = NULL;
+//BLECharacteristic* pCharacteristicM = NULL;
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
 
 int32_t loopCnt = 0;
 
 Adafruit_BME280 bme; // I2C
+int bBmePresent = 0;
 
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
 
 #define SERVICE_UUID        "2acd1159-5703-4b2d-95be-ac963748c40c"
 #define CHARACTERISTIC_T_UUID "9b5099ae-10f0-4a78-a8b7-eb086e3cb69b"
-#define CHARACTERISTIC_P_UUID "d2bc7047-ef44-4c5f-9913-3ee6a174e44a"
-#define CHARACTERISTIC_H_UUID "1c383a81-5387-41bc-b55f-3ff410da6f7e"
-#define CHARACTERISTIC_B_UUID "2af553d1-3f9f-4b19-ac6c-82ced3255cd0"
-#define CHARACTERISTIC_M_UUID "ea936dd5-eafa-4f70-9b7a-6a794a4de3a9"
+//#define CHARACTERISTIC_P_UUID "d2bc7047-ef44-4c5f-9913-3ee6a174e44a"
+//#define CHARACTERISTIC_H_UUID "1c383a81-5387-41bc-b55f-3ff410da6f7e"
+//#define CHARACTERISTIC_B_UUID "2af553d1-3f9f-4b19-ac6c-82ced3255cd0"
+//#define CHARACTERISTIC_M_UUID "ea936dd5-eafa-4f70-9b7a-6a794a4de3a9"
 
 
 class MyServerCallbacks: public BLEServerCallbacks {
@@ -83,15 +89,16 @@ void setup() {
     Serial.println(F("No valid BME280 sensor")); //, check wiring or "
 //                      "try a different address!"));
                       
-    Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(),16);
-    Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
-    Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
-    Serial.print("        ID of 0x60 represents a BME 280.\n");
-    Serial.print("        ID of 0x61 represents a BME 680.\n");
+//    Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(),16);
+//    Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+//    Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
+//    Serial.print("        ID of 0x60 represents a BME 280.\n");
+//    Serial.print("        ID of 0x61 represents a BME 680.\n");
 //    while (1) delay(10);
     
   }
   else {
+    bBmePresent = 1;
     // Default settings from datasheet. 
     bme.setSampling(Adafruit_BME280::MODE_NORMAL,   // Operating Mode. 
                   Adafruit_BME280::SAMPLING_X2,     // Temp. oversampling 
@@ -121,7 +128,7 @@ void setup() {
   // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
   // Create a BLE Descriptor
   pCharacteristicT->addDescriptor(new BLE2902());
-
+/*
   // Create a BLE Characteristic
   pCharacteristicP = pService->createCharacteristic(
                       CHARACTERISTIC_P_UUID,
@@ -148,13 +155,13 @@ void setup() {
                       CHARACTERISTIC_M_UUID,
                       BLECharacteristic::PROPERTY_NOTIFY
                     );
-
+*/
   // Create a BLE Descriptor
   pCharacteristicT->addDescriptor(new BLE2902());
-  pCharacteristicP->addDescriptor(new BLE2902());
-  pCharacteristicH->addDescriptor(new BLE2902());
-  pCharacteristicB->addDescriptor(new BLE2902());
-  pCharacteristicM->addDescriptor(new BLE2902());
+//  pCharacteristicP->addDescriptor(new BLE2902());
+//  pCharacteristicH->addDescriptor(new BLE2902());
+//  pCharacteristicB->addDescriptor(new BLE2902());
+//  pCharacteristicM->addDescriptor(new BLE2902());
 
   // Start the service
   pService->start();
@@ -171,12 +178,15 @@ void setup() {
 void loop() {
     // notify changed value
     if (deviceConnected) {
-        delay(5000); 
+        float fT = 0;
+        float fPa = 0;
+        float fHu = 0;
 
-        float fT = bme.readTemperature();
-        float fPa = bme.readPressure() / 100;
-        float fHu = bme.readHumidity();
-
+        if (bBmePresent) {
+          fT = bme.readTemperature();
+          fPa = bme.readPressure() / 100;
+          fHu = bme.readHumidity();
+        }
         //Serial.print("T = ");
         //Serial.println(fT);
         //Serial.print("P = ");
@@ -184,14 +194,25 @@ void loop() {
         //Serial.print("H = ");
         //Serial.println(fHu);
 
-        delay(100); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
-        float fV = 2.0 * 3.67 * analogRead(34) / 4095;
+        float fV = 2.0 * 3.67 * analogRead(BATTERY_IO) / 4095;
         //Serial.println(fV);
 
-        delay(100); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
-        int msensor = analogRead(35);  // Read the analog value from sensor
-        float fM = map(msensor, MMIN, MMAX, 100, 0); // map the 14-bit data to 8-bit data
+        int msensor = 0;
+        for (int i = 0; i < MSENSOR_N; i++)
+        {
+          msensor += analogRead(MSENSOR_IO);  // Read the analog value from sensor
+          delay(100);
+        }
+        float fM = map(msensor/MSENSOR_N, MMIN, MMAX, 100, 0); // map the 14-bit data to 8-bit data
 
+        float fNotify[] = {fT, fPa, fHu, fV, fM}; 
+        pCharacteristicT->setValue((uint8_t*)&fNotify, sizeof(fNotify));
+
+        delay(3000); 
+
+        pCharacteristicT->notify();
+
+/*
         pCharacteristicT->setValue((uint8_t*)&fT, sizeof(fT));
         pCharacteristicT->notify();
         pCharacteristicP->setValue((uint8_t*)&fPa, sizeof(fPa));
@@ -202,9 +223,9 @@ void loop() {
         pCharacteristicB->notify();
         pCharacteristicM->setValue((uint8_t*)&fM, sizeof(fM));
         pCharacteristicM->notify();
+*/
 
-
-        delay(2000); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
+        delay(500);
         
         esp_deep_sleep_start();
 //        deviceConnected = false;
@@ -215,7 +236,7 @@ void loop() {
     else
     {
         // waiting for connection but not for too long
-        delay(500); 
+        delay(100); 
         if (loopCnt > MAX_CONNECT_WAIT)
         {
           esp_deep_sleep_start();
